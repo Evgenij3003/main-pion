3b871936ff6dceaa02534625c589df1c72ee61ad
Evgenij3003 <97961367+Evgenij3003@users.noreply.github.com>
2024-04-11 17:36:29 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1025,6 +1025,7 @@ if (document.querySelector(".nav-schedule")) {
     function setPositionLine(activeButton, scroll = false) {
         leftPosition = activeButton.offsetLeft;
         let addValue = window.innerWidth > 1023.98 ? 6 : 7;
+        alert(addValue);
         // scroll ? lineActive.style.transitionDuration = "0s" : lineActive.style.transitionDuration = "0.3s";
         lineActive.style.left = `${(leftPosition + addValue) / 16}rem`;
     }
