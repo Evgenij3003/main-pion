e2aa425a6c5d3b8fb0456d34b42f58091cd189e1
Evgenij3003 <97961367+Evgenij3003@users.noreply.github.com>
2024-04-11 18:52:11 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1048,7 +1048,7 @@ if (document.querySelector(".nav-schedule")) {
     // Функция расчета положения элементов месяцев:
     function setPositionMonths() {
         var monthBlock = document.querySelector(".nav-schedule__month");
-        var monthElements = document.querySelectorAll(".nav-schedule__month span");
+        const monthElements = document.querySelectorAll(".nav-schedule__month span");
         monthBlock.style.width = `${(firstDatePosition - 20) / 16}rem`;
 
         monthElements.forEach(monthElement => {
